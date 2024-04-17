428043b47810d7d01bc30499b5f0852dfc264253
parth0301 <99258522+parth0301@users.noreply.github.com>
2024-04-17 11:03:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-// src/App.jsx
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import ContributionContext from './ContributionContext';
+import RecipeContext from './RecipeContext';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import RecipeDetail from './pages/RecipeDetail';
+import AboutUs from './pages/AboutUs';
 import Contribute from './pages/Contribute';
 import Favorites from './pages/Favorites';
-import AboutUs from './pages/AboutUs';
-import RecipeContext from './RecipeContext';
+import Home from './pages/Home';
+import LoginSignup from './pages/LoginSignup';
+import MyContributions from './pages/MyContributions';
+import RecipeDetail from './pages/RecipeDetail';
 
 const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [favorites, setFavorites] = useState([]);
-
+  const [userContribution, setUserContribution] = useState(null);
   return (
     <RecipeContext.Provider value={{ recipes, setRecipes, favorites, setFavorites}}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/recipe-detail" element={<RecipeDetail />} />
-          <Route path="/contribute" element={<Contribute />} />
-          <Route path="/favorites" element={<Favorites />} />
-          <Route path="/about-us" element={<AboutUs />} />
-        </Routes>
-      </Router>
+      <ContributionContext.Provider value={{ userContribution, setUserContribution }}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<LoginSignup />} />
+            <Route exact path="/home" element={<Home />} />
+            <Route path="/recipe-detail" element={<RecipeDetail />} />
+            <Route path="/contribute" element={<Contribute />} />
+            <Route path="/my-contributions" element={<MyContributions />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/about-us" element={<AboutUs />} />
+          </Routes>
+        </Router>
+      </ContributionContext.Provider>
     </RecipeContext.Provider>
   );
 };
