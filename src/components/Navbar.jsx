480e4499c28d527db1cd867ad9552a24afef3270
parth0301 <99258522+parth0301@users.noreply.github.com>
2024-04-17 10:53:29 +0530

    Prev navbar was eh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-// src/components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -9,6 +8,7 @@ const Navbar = () => {
       <Link to="/">Home</Link>
       <Link to="/recipe-detail">Recipe Detail</Link>
       <Link to="/contribute">Contribute</Link>
+      <Link to="/my-contributions">My Contributions</Link>
       <Link to="/favorites">Favorites</Link>
       <Link to="/about-us">About Us</Link>
     </nav>
